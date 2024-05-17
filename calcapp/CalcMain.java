c9ff16674ac95e3ccdfb46f12413e0624bf88eba
annbaraasuv <anbaraasu@hotmail.com>
2024-05-17 10:18:46 +0530

    Delete unused files and code related to climate data service and repository

---
+++ calcapp/CalcMain.java
@@ -0,0 +1,1 @@
+//java app with menu add, sub, mul, div and exit options, with input from user with excpetion handling of input mismatch
---
+++ climatechart/Prime.cpp
@@ -0,0 +1,3 @@
+//Program for Bank EMI calculation
+//EMI is the equated monthly installment
+// EMI = [P x R x (1+R)^N]/[(1+R)^N-1], where P stands for the loan amount or principal, R is the interest rate per month, and N is the number of monthly installments.
